261b4a7e378fcc9164cc489d5820817b87a6c9c2
glider <glider1967@protonmail.com>
2024-07-10 03:30:49 +0900

    chore: dto

--- src/posts/dto/update-post.dto.ts
+++ src/posts/dto/update-post.dto.ts
@@ -1,7 +1,4 @@
-import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { PartialType } from '@nestjs/swagger';
 import { CreatePostDto } from './create-post.dto';
 
-export class UpdatePostDto extends PartialType(CreatePostDto) {
-  @ApiProperty()
-  content: string;
-}
+export class UpdatePostDto extends PartialType(CreatePostDto) {}
--- src/users/dto/update-user.dto.ts
+++
@@ -1,7 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateUserDto } from './create-user.dto';
-
-export class UpdateUserDto extends PartialType(CreateUserDto) {
-  name: string;
-  password: string;
-}

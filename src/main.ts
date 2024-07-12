dc5ead5b38d6a5132a4f97c65da34a6504f340d1
glider <glider1967@protonmail.com>
2024-07-12 20:27:12 +0900

    auto generate swagger yml

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as fs from 'fs';
+import { dump } from 'js-yaml';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +14,8 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  fs.writeFileSync('./swagger-spec.yaml', dump(document, {}));
 
   await app.listen(3000);
 }
